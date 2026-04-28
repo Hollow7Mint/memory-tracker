679f22ff9d755b48645bebb4a98185dbae526b66
James Holloway <james.holloway94@gmail.com>
2026-04-28 09:13:06 +0000

    persist connection string refresh in config banner

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-04-28 08:47:17 UTC
+# Last sync: 2026-04-28 09:13:06 UTC
