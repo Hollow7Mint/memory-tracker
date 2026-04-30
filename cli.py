9a527f84fa90bfdad6663f457e5cb4703052378f
James Holloway <james.holloway94@gmail.com>
2026-04-30 05:53:10 +0000

    finish mongodb sharding sync heartbeat after balance

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-04-30 05:03:31 UTC
+# Last sync: 2026-04-30 05:53:10 UTC
