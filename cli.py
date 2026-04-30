0c4f20f9982b3831bea03545a27ffef36abfae08
James Holloway <james.holloway94@gmail.com>
2026-04-30 14:41:41 +0000

    index s3 cors policy sync during audit pass

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-04-30 13:15:29 UTC
+# Last sync: 2026-04-30 14:41:41 UTC
