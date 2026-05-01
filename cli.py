67b10ce8a32dc424cacfedc2b5d1b7479c3ccb41
James Holloway <james.holloway94@gmail.com>
2026-05-01 14:36:56 +0000

    seal bastion session sync in security banner

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-01 13:39:43 UTC
+# Last sync: 2026-05-01 14:36:56 UTC
