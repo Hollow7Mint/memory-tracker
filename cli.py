d916570636e188395694d5de1201e4be9c3be1ba
James Holloway <james.holloway94@gmail.com>
2026-05-05 12:26:48 +0000

    inspect bastion port forward sync for security

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-05 11:56:18 UTC
+# Last sync: 2026-05-05 12:26:48 UTC
