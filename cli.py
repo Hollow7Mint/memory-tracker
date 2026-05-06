04225203ca77dcd67ab8d7681af23dbaec000054
James Holloway <james.holloway94@gmail.com>
2026-05-06 16:23:44 +0000

    document postgres index maintenance sync in record

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-06 16:04:41 UTC
+# Last sync: 2026-05-06 16:23:43 UTC
