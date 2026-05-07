9508739df7273cd0b4ec1f0e4e2f1431feaf11da
James Holloway <james.holloway94@gmail.com>
2026-05-07 19:15:25 +0000

    preserve nat gateway sync time in infra banner

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-07 18:43:38 UTC
+# Last sync: 2026-05-07 19:15:25 UTC
