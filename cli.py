024429d9a5d4e36ad2a9ca295593e3e8498d5365
James Holloway <james.holloway94@gmail.com>
2026-05-12 18:05:13 +0000

    stage postgres tablespace sync for scheduled refresh

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-12 16:40:55 UTC
+# Last sync: 2026-05-12 18:05:13 UTC
