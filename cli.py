8b69dbc75d95fb12289235a885b9370cdea8bcbf
James Holloway <james.holloway94@gmail.com>
2026-05-14 17:02:00 +0000

    log shield ddos event time in protection banner

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-14 15:20:02 UTC
+# Last sync: 2026-05-14 17:02:00 UTC
