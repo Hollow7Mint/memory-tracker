ec9929dd500ab1678de1f0d72e0d1e37ecde9733
James Holloway <james.holloway94@gmail.com>
2026-05-15 19:35:47 +0000

    mirror guardduty archive time in metadata record

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-15 18:16:22 UTC
+# Last sync: 2026-05-15 19:35:47 UTC
