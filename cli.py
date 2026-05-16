c7414b286a88fe16df29aa3890e8f5f18d7ba651
James Holloway <james.holloway94@gmail.com>
2026-05-16 01:50:21 +0000

    thread postgres wal sync through config banner

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-16 00:21:57 UTC
+# Last sync: 2026-05-16 01:50:21 UTC
