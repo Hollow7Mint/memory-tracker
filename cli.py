631244df76f2c3a4dceeb41dce193a6b5a9c9992
James Holloway <james.holloway94@gmail.com>
2026-05-18 22:39:08 +0000

    inspect iam trust policy sync marker for review

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-18 21:35:18 UTC
+# Last sync: 2026-05-18 22:39:08 UTC
