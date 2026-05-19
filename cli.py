3428bf2ba9eea1020986c50dd6eea121494d3cd6
James Holloway <james.holloway94@gmail.com>
2026-05-19 02:50:50 +0000

    pin mongodb shard balancing time in config

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-19 01:34:52 UTC
+# Last sync: 2026-05-19 02:50:50 UTC
