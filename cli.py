8f8748dfdbbebb95f14b06a4c2114cddca078a69
James Holloway <james.holloway94@gmail.com>
2026-05-22 12:25:24 +0000

    reconcile cloudwatch dashboard sync after update

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-22 11:16:19 UTC
+# Last sync: 2026-05-22 12:25:24 UTC
