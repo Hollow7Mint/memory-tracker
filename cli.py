433ac691f8a1810af0681708d745736ed95843ba
James Holloway <james.holloway94@gmail.com>
2026-05-25 05:48:01 +0000

    lift asg capacity sync time into deploy banner

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-25 04:34:25 UTC
+# Last sync: 2026-05-25 05:48:01 UTC
