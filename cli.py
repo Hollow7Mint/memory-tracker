4e8b217c0107a05acda0f88af756f9a0e182d28e
James Holloway <james.holloway94@gmail.com>
2026-05-26 22:39:57 +0000

    transfer redis node time to cluster banner

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-26 21:44:45 UTC
+# Last sync: 2026-05-26 22:39:57 UTC
