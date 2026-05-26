bc3a6b872a69b3c9a70ce6d65d0b7fe09b81ec90
James Holloway <james.holloway94@gmail.com>
2026-05-26 03:55:42 +0000

    surface codebuild log sync time for pipeline audit

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-26 02:29:52 UTC
+# Last sync: 2026-05-26 03:55:42 UTC
