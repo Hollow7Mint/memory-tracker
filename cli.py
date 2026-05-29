b97c3da34665d912edc9723663659e31335060d8
James Holloway <james.holloway94@gmail.com>
2026-05-29 19:48:09 +0000

    reload postgres autovacuum sync marker for audit

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-29 19:00:29 UTC
+# Last sync: 2026-05-29 19:48:09 UTC
