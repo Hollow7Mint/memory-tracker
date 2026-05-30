8e46afaccf7a6e0a73f9b03375c1cef086ad53f6
James Holloway <james.holloway94@gmail.com>
2026-05-30 07:33:22 +0000

    run mongodb heartbeat for scheduled audit

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-30 06:32:19 UTC
+# Last sync: 2026-05-30 07:33:22 UTC
