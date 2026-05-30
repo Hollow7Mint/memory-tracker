1d638a76ca7b7c9f985aa9932fedbd5b98bae27d
James Holloway <james.holloway94@gmail.com>
2026-05-30 22:29:45 +0000

    save bastion policy sync marker after review

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-30 21:47:33 UTC
+# Last sync: 2026-05-30 22:29:45 UTC
