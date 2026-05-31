56ad766c904ad900872f005c49866012cc7a6db3
James Holloway <james.holloway94@gmail.com>
2026-05-31 09:16:40 +0000

    publish lambda alias promotion marker after release

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-31 08:18:50 UTC
+# Last sync: 2026-05-31 09:16:40 UTC
