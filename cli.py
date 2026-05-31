acb15a0b5f15f903960a723174b2eed4434cf57e
James Holloway <james.holloway94@gmail.com>
2026-05-31 08:18:50 +0000

    document alb target health sync for nightly check

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-31 07:37:13 UTC
+# Last sync: 2026-05-31 08:18:50 UTC
