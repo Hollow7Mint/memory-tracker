30a09a4ffc14338d5c58e3e93d7e19c17a61d715
James Holloway <james.holloway94@gmail.com>
2026-06-04 10:03:25 +0000

    emit ssh certificate renewal heartbeat for audit

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-04 08:33:13 UTC
+# Last sync: 2026-06-04 10:03:25 UTC
