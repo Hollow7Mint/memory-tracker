3ffd5c2f4fc1480ea9ea3e85f2c83661728ea03e
James Holloway <james.holloway94@gmail.com>
2026-06-07 10:54:39 +0000

    register waf acl update time for security trail

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-07 09:14:57 UTC
+# Last sync: 2026-06-07 10:54:39 UTC
