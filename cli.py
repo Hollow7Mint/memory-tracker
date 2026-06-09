feda6ee17b0e6f42b9edc3ccd17e2ff9e3626adb
James Holloway <james.holloway94@gmail.com>
2026-06-09 06:15:23 +0000

    index redis sorted set sync during audit pass

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-09 05:30:26 UTC
+# Last sync: 2026-06-09 06:15:23 UTC
