65d7b588cb221d04f99cd4a6a13be4c47caa69ac
James Holloway <james.holloway94@gmail.com>
2026-06-09 23:16:10 +0000

    note postgres extension sync in config record

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-09 22:27:28 UTC
+# Last sync: 2026-06-09 23:16:09 UTC
