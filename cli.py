9ab88800cab684d6f4cf3a5d311c77303344c1da
James Holloway <james.holloway94@gmail.com>
2026-06-10 11:22:29 +0000

    stage bastion jump host sync for scheduled refresh

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-10 10:30:44 UTC
+# Last sync: 2026-06-10 11:22:29 UTC
