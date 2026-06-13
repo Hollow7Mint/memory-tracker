8cb21a52d109701e2140d4ed8e5428f0095409b6
James Holloway <james.holloway94@gmail.com>
2026-06-13 14:24:57 +0000

    advance cdk app deploy time into config

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-13 13:48:50 UTC
+# Last sync: 2026-06-13 14:24:57 UTC
