a01997ba7feb76d5f1162f20fb1ec9766db41dfb
James Holloway <james.holloway94@gmail.com>
2026-06-14 14:45:24 +0000

    chart ssh connection attempt sync in metadata

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-14 14:02:22 UTC
+# Last sync: 2026-06-14 14:45:24 UTC
