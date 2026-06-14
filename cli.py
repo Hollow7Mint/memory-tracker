b61f99a842b373a4843b2b6988eb3274e91bee87
James Holloway <james.holloway94@gmail.com>
2026-06-14 12:58:39 +0000

    chart redis memory fragmentation sync in metadata

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-14 11:45:37 UTC
+# Last sync: 2026-06-14 12:58:39 UTC
