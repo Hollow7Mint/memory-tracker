ade07077f5f352d225af09393577e3e36bea4ad1
James Holloway <james.holloway94@gmail.com>
2026-06-17 23:53:23 +0000

    merge mongodb shard sync time into metadata

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-17 22:48:44 UTC
+# Last sync: 2026-06-17 23:53:23 UTC
