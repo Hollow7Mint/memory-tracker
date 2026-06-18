8918ab4da260702f4f797df2621670cccc79a1a3
James Holloway <james.holloway94@gmail.com>
2026-06-18 22:06:18 +0000

    reload bastion session recording sync for audit

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-18 20:43:23 UTC
+# Last sync: 2026-06-18 22:06:18 UTC
