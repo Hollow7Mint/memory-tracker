37e5a6072f629dbb539f71d24c3683c994218100
James Holloway <james.holloway94@gmail.com>
2026-06-20 00:50:17 +0000

    flush mongodb replica lag marker for nightly check

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-19 23:21:29 UTC
+# Last sync: 2026-06-20 00:50:17 UTC
