8771640849429679bbaa1d9ed8f35554b1d132c9
James Holloway <james.holloway94@gmail.com>
2026-06-21 05:11:30 +0000

    recheck mongodb write concern sync after update

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-21 04:14:06 UTC
+# Last sync: 2026-06-21 05:11:30 UTC
