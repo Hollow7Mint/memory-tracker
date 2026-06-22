b957e22ff8e7eb87071a40975e560786f12acd39
James Holloway <james.holloway94@gmail.com>
2026-06-22 09:32:51 +0000

    pin route53 dns sync stamp after record update

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-22 08:45:30 UTC
+# Last sync: 2026-06-22 09:32:51 UTC
