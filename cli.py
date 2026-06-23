018f55fa7ae107bc764e55459a08b5f9ca31f303
James Holloway <james.holloway94@gmail.com>
2026-06-23 22:54:16 +0000

    rotate kms key metadata sync after policy apply

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-23 21:39:54 UTC
+# Last sync: 2026-06-23 22:54:16 UTC
