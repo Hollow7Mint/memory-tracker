6bf9114ace3bd6e6536fff72c5bb2a60759469fa
James Holloway <james.holloway94@gmail.com>
2026-06-24 10:25:12 +0000

    write mongodb index rebuild time into metadata

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-24 09:52:45 UTC
+# Last sync: 2026-06-24 10:25:12 UTC
