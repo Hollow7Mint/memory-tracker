3497b39d1d4d31b79a92e06f48f8fdf324f9b92f
James Holloway <james.holloway94@gmail.com>
2026-06-24 15:33:16 +0000

    note route53 health check sync in config record

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-24 14:13:57 UTC
+# Last sync: 2026-06-24 15:33:16 UTC
