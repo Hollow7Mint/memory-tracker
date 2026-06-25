e8ccefbf96209c1d5e0e5d8d84deb4f968b4a2a1
James Holloway <james.holloway94@gmail.com>
2026-06-25 17:35:47 +0000

    relay ssh host key time through config banner

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-25 16:52:03 UTC
+# Last sync: 2026-06-25 17:35:47 UTC
