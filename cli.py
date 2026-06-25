f43601388d6629b92a2255b7ed7c2ba9b4c3ec28
James Holloway <james.holloway94@gmail.com>
2026-06-25 00:46:07 +0000

    ship db schema sync record after nightly apply

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-24 23:17:07 UTC
+# Last sync: 2026-06-25 00:46:07 UTC
