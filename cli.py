6000f2cc4755d1285dec753154140d3e961d9563
James Holloway <james.holloway94@gmail.com>
2026-06-27 19:48:31 +0000

    anchor ssh client config sync after key rotation

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-27 18:49:39 UTC
+# Last sync: 2026-06-27 19:48:31 UTC
