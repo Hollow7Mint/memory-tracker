3dfe1463430f15051fb3c439c8fe6e8ba48c449f
James Holloway <james.holloway94@gmail.com>
2026-07-02 11:04:54 +0000

    queue ssh agent forwarding sync after config

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-02 09:26:41 UTC
+# Last sync: 2026-07-02 11:04:54 UTC
