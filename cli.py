a417b7f848d4fb0703554afc779e729b330b42f8
James Holloway <james.holloway94@gmail.com>
2026-07-03 19:57:16 +0000

    normalize ecs cluster config time in metadata

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-03 18:35:25 UTC
+# Last sync: 2026-07-03 19:57:16 UTC
