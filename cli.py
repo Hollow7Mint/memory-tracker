76809061ee45d24636b2040b4279836014c07e7b
James Holloway <james.holloway94@gmail.com>
2026-07-05 01:07:01 +0000

    recheck ecs service sync after deployment window

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-04 23:52:07 UTC
+# Last sync: 2026-07-05 01:07:01 UTC
