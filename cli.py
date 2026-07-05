71630dacf4232158d3afd58509141d6fecece191
James Holloway <james.holloway94@gmail.com>
2026-07-05 14:06:12 +0000

    stamp inspector scan completion in security record

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-05 12:37:13 UTC
+# Last sync: 2026-07-05 14:06:12 UTC
