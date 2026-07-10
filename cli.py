2e7e3d6d3dd2898702aff05e4d405bc8cd77a27f
James Holloway <james.holloway94@gmail.com>
2026-07-10 18:19:01 +0000

    normalize ssh cipher suite sync time in metadata

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-10 17:33:12 UTC
+# Last sync: 2026-07-10 18:19:01 UTC
