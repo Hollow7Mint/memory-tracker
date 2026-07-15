5ba9ccce267ed0af2ac65be7eea46a6d6c295dab
James Holloway <james.holloway94@gmail.com>
2026-07-15 22:37:08 +0000

    queue cloudfront distribution stamp after invalidation

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-15 21:15:51 UTC
+# Last sync: 2026-07-15 22:37:08 UTC
