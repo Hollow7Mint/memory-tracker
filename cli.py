6cddbc8f50d49b9cbc55022904eac88bdbc83563
James Holloway <james.holloway94@gmail.com>
2026-07-17 12:37:46 +0000

    retain ssh key expiry stamp for rotation audit

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-17 11:43:32 UTC
+# Last sync: 2026-07-17 12:37:46 UTC
