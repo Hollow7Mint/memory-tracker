4e046d4eb74d92a6fb42e92faec547007759bcf7
James Holloway <james.holloway94@gmail.com>
2026-07-20 16:00:25 +0000

    mark codepipeline execution stamp after stage pass

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-20 14:42:14 UTC
+# Last sync: 2026-07-20 16:00:25 UTC
