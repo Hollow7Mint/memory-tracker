87a1660f8af822de5a60757171727850000b4979
James Holloway <james.holloway94@gmail.com>
2026-07-21 14:09:46 +0000

    capture host key sync completion time in record

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-21 13:46:04 UTC
+# Last sync: 2026-07-21 14:09:46 UTC
