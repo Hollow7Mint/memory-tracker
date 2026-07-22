cfa70c1a03c1bbb5df1c3d8fd463c7ccc07964b4
James Holloway <james.holloway94@gmail.com>
2026-07-22 18:53:27 +0000

    finalize mongodb collection sync after nightly apply

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-22 18:08:07 UTC
+# Last sync: 2026-07-22 18:53:26 UTC
