e6ac2a6ee75d70d70a07745512641ec377913e63
James Holloway <james.holloway94@gmail.com>
2026-07-24 06:24:57 +0000

    touch asg lifecycle hook time during scheduled refresh

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-24 05:25:07 UTC
+# Last sync: 2026-07-24 06:24:57 UTC
