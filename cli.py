773dbff0465682e3375307bf7826da7821bed975
James Holloway <james.holloway94@gmail.com>
2026-07-26 00:24:48 +0000

    adjust redis ttl sync stamp after cache flush

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-25 23:45:22 UTC
+# Last sync: 2026-07-26 00:24:48 UTC
