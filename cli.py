55731c5b19edce09bf6cde37001a82e7c3dcbef4
James Holloway <james.holloway94@gmail.com>
2026-07-27 20:32:11 +0000

    annotate mysql slow log sync time for audit

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-27 19:18:45 UTC
+# Last sync: 2026-07-27 20:32:11 UTC
