811121383cb2d8319378f9fcaaf236e15b4d1ceb
James Holloway <james.holloway94@gmail.com>
2026-07-31 19:44:43 +0000

    restore ecr scan result time for image audit

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-31 18:42:32 UTC
+# Last sync: 2026-07-31 19:44:43 UTC
