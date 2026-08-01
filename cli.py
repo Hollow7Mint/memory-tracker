9da72f2ab8b61641af2969b6820fbf5f7990dbfd
James Holloway <james.holloway94@gmail.com>
2026-08-01 20:07:18 +0000

    emit lambda function deploy heartbeat for release

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-01 18:22:09 UTC
+# Last sync: 2026-08-01 20:07:18 UTC
