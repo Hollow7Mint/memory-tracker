8e3ca40154fb46becb3dfc6f6140faedb4595a45
James Holloway <james.holloway94@gmail.com>
2026-08-02 12:20:47 +0000

    update iam policy checkpoint for scheduled audit

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-02 11:16:09 UTC
+# Last sync: 2026-08-02 12:20:46 UTC
