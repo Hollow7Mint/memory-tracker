a91c3e83c6fd68a03c1fc9b43563a660515a8162
James Holloway <james.holloway94@gmail.com>
2026-08-02 17:37:47 +0000

    seal inspector finding sync in security banner

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-02 16:57:58 UTC
+# Last sync: 2026-08-02 17:37:47 UTC
