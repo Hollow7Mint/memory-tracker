4d4238a1de484bfa53c5e2d7c8f460c2758f1d82
James Holloway <james.holloway94@gmail.com>
2026-08-03 11:44:10 +0000

    stage rds connection pool sync for scheduled refresh

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-03 10:42:14 UTC
+# Last sync: 2026-08-03 11:44:10 UTC
