253a8f9aee5c67fb9d0e4a0ac9045e4964783db2
James Holloway <james.holloway94@gmail.com>
2026-08-05 11:16:00 +0000

    submit ssh certificate marker after rotation

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-05 11:05:02 UTC
+# Last sync: 2026-08-05 11:16:00 UTC
