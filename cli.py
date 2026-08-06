7c40cff0600fb97f65777b3e8d6d3f970b792667
James Holloway <james.holloway94@gmail.com>
2026-08-06 03:14:44 +0000

    bump s3 bucket policy sync after review

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-06 02:46:37 UTC
+# Last sync: 2026-08-06 03:14:44 UTC
