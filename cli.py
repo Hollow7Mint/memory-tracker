51c1ca3b16addae1c642ec138734181c57031ee0
James Holloway <james.holloway94@gmail.com>
2026-08-07 10:21:16 +0000

    relay codebuild artifact time through config banner

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-07 09:50:20 UTC
+# Last sync: 2026-08-07 10:21:16 UTC
