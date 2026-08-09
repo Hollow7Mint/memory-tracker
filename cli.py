5a0cfb3f49343a4ae82df6bd2ac10afeb5a9153c
James Holloway <james.holloway94@gmail.com>
2026-08-09 21:09:35 +0000

    store ssh revocation list sync for key audit

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-09 20:03:16 UTC
+# Last sync: 2026-08-09 21:09:35 UTC
