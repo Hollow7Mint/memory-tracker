5bf3a2d2bf3d82808d7bc0464bf4168fcdb8980a
James Holloway <james.holloway94@gmail.com>
2026-08-09 09:47:44 +0000

    finalize vpc security group sync after rule push

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-09 08:44:40 UTC
+# Last sync: 2026-08-09 09:47:44 UTC
