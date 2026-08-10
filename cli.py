0f8ba1dcecbaeea43fdeef19939f635c3f9fec8a
James Holloway <james.holloway94@gmail.com>
2026-08-10 19:39:35 +0000

    push mysql connection pool sync after refresh

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-10 18:40:31 UTC
+# Last sync: 2026-08-10 19:39:35 UTC
