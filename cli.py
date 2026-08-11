a03230fe049f7dac85ca93bfbef18bdcea3ca34b
James Holloway <james.holloway94@gmail.com>
2026-08-11 21:10:36 +0000

    push ssh config sync marker after host refresh

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-11 20:15:33 UTC
+# Last sync: 2026-08-11 21:10:36 UTC
