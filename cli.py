6cf2ff11c4eeafb3373f6dbb6eb8c1acef542df5
James Holloway <james.holloway94@gmail.com>
2026-08-14 10:13:19 +0000

    unify ssh sync record with metadata heartbeat

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-14 09:36:45 UTC
+# Last sync: 2026-08-14 10:13:19 UTC
