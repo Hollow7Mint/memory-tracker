4ad6be77fa40f19efd32c404aecc12e84b4e9af3
James Holloway <james.holloway94@gmail.com>
2026-08-17 17:58:12 +0000

    inspect postgres lock wait sync for performance

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-17 16:11:08 UTC
+# Last sync: 2026-08-17 17:58:12 UTC
