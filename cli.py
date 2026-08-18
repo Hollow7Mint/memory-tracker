e2ab5ae876e7a9689e30c41f2c1b78d844698698
James Holloway <james.holloway94@gmail.com>
2026-08-18 17:16:35 +0000

    post postgres replication heartbeat to config

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-18 16:49:01 UTC
+# Last sync: 2026-08-18 17:16:35 UTC
