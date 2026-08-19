193592197da6842e30fdc677db8ef096d6f5dd24
James Holloway <james.holloway94@gmail.com>
2026-08-19 03:10:04 +0000

    adjust bastion acl sync stamp after ip review

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-19 02:24:47 UTC
+# Last sync: 2026-08-19 03:10:03 UTC
