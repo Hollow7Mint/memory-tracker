5c2a520f2754a382876cd41eeddbd61b10d4eb1c
James Holloway <james.holloway94@gmail.com>
2026-08-19 04:57:59 +0000

    refresh aws config sync marker after nightly reconcile

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-19 03:10:03 UTC
+# Last sync: 2026-08-19 04:57:59 UTC
