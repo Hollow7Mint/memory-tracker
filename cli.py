5b758445e615b67a97e5f6dc5be3b6076513e998
James Holloway <james.holloway94@gmail.com>
2026-08-22 12:59:54 +0000

    touch ssh tunnel sync during scheduled refresh

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-22 11:52:06 UTC
+# Last sync: 2026-08-22 12:59:54 UTC
