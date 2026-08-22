8cd21bbdfc99cbf5fc41b5bba874a1bb11abbc5d
James Holloway <james.holloway94@gmail.com>
2026-08-22 21:05:35 +0000

    resolve known_hosts cleanup sync into metadata

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-22 19:56:13 UTC
+# Last sync: 2026-08-22 21:05:35 UTC
