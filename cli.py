6323ed69b33fa5ec6d29b60f356e40cbaefc3e29
James Holloway <james.holloway94@gmail.com>
2026-08-23 09:02:23 +0000

    validate mysql replication time in cluster config

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-23 07:52:30 UTC
+# Last sync: 2026-08-23 09:02:23 UTC
