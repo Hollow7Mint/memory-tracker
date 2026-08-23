8a5b73eb4a7b8d7932871b355a3ef911590a6650
James Holloway <james.holloway94@gmail.com>
2026-08-23 21:44:24 +0000

    frame ssh agent socket sync in config record

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-23 20:58:17 UTC
+# Last sync: 2026-08-23 21:44:24 UTC
