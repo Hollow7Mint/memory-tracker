3ea113223490157a8ebf8243e687bb1c9a5cfe8c
James Holloway <james.holloway94@gmail.com>
2026-08-25 12:33:46 +0000

    publish ssh key distribution sync after rotation

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-25 11:46:12 UTC
+# Last sync: 2026-08-25 12:33:46 UTC
