6f0c5a41993e9e51f2cc5770f20f3e4977a1cdec
James Holloway <james.holloway94@gmail.com>
2026-08-25 02:14:12 +0000

    post cdk bootstrap sync stamp to config

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-25 01:34:45 UTC
+# Last sync: 2026-08-25 02:14:12 UTC
