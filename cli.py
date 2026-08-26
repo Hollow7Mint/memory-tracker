73949adbd5610ca888a6697052e7250790f14e1b
James Holloway <james.holloway94@gmail.com>
2026-08-26 17:35:49 +0000

    tune cfn change set completion stamp for audit

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-26 16:53:17 UTC
+# Last sync: 2026-08-26 17:35:48 UTC
