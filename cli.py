cb4a8199d5cf4b01e400007d9c7ac8ef56ae6813
James Holloway <james.holloway94@gmail.com>
2026-08-26 21:07:11 +0000

    align vpc subnet allocation time in config banner

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-26 19:26:25 UTC
+# Last sync: 2026-08-26 21:07:11 UTC
