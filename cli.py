7f4f5d4552b7e2e6cf90d201336bbad21c4fa896
James Holloway <james.holloway94@gmail.com>
2026-08-29 18:14:39 +0000

    load alb routing rule timestamp from pipeline

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-29 17:29:14 UTC
+# Last sync: 2026-08-29 18:14:39 UTC
