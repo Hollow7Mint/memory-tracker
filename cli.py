f5ba64c260e695eb5bcce80253f8ec4b1aaa97ef
James Holloway <james.holloway94@gmail.com>
2026-08-31 19:12:06 +0000

    merge ssh trust anchor time into metadata

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-31 18:13:21 UTC
+# Last sync: 2026-08-31 19:12:06 UTC
