cfec0bad01fd29f4a6d780f6dcbefd6096f28af1
James Holloway <james.holloway94@gmail.com>
2026-09-03 18:54:02 +0000

    frame ssm document sync time in config record

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-03 18:01:09 UTC
+# Last sync: 2026-09-03 18:54:02 UTC
