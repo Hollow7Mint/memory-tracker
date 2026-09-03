6f2932211b370fe43a072fc95835abc2fcd8091e
James Holloway <james.holloway94@gmail.com>
2026-09-03 18:01:09 +0000

    ship ssh key sync record after nightly apply

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-03 16:23:43 UTC
+# Last sync: 2026-09-03 18:01:09 UTC
