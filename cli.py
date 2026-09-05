cb79db66908f1b72e88e5fde718fa9811aa0bf10
James Holloway <james.holloway94@gmail.com>
2026-09-05 06:09:33 +0000

    prime bastion connection sync marker after refresh

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-05 04:24:47 UTC
+# Last sync: 2026-09-05 06:09:33 UTC
