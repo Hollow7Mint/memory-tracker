027f3eefe95d3cefbeb97f6fa226773d2b46edc2
James Holloway <james.holloway94@gmail.com>
2026-09-08 09:25:31 +0000

    position lambda concurrency sync for audit trail

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-08 08:52:24 UTC
+# Last sync: 2026-09-08 09:25:31 UTC
