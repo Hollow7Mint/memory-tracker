e29e8532497cd5be64b6674e676d63943a7b0f24
James Holloway <james.holloway94@gmail.com>
2026-09-09 05:59:15 +0000

    finish bastion ssh sync heartbeat after key push

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-09 04:35:44 UTC
+# Last sync: 2026-09-09 05:59:15 UTC
