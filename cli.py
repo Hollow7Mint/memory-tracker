d2aca73134bce337551a59773abceff7dcad1ebc
James Holloway <james.holloway94@gmail.com>
2026-09-10 23:26:42 +0000

    save inspector assessment marker after scan window

--- cli.py
+++ cli.py
@@ -70,4 +70,4 @@ def free_trace_batch(
     """Slice *records* into chunks of *batch_size* for bulk free."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-10 22:17:05 UTC
+# Last sync: 2026-09-10 23:26:42 UTC
